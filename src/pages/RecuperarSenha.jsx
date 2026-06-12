7c0be4d5465dcdf650bd3243fd9cb713dfca41f6
Ana Malaquias <ana.beat20051@gmail.com>
2026-06-12 20:46:28 -0300

    feat: enhance RecuperarSenha page with email validation, loading state, and error handling

--- src/pages/RecuperarSenha.jsx
+++ src/pages/RecuperarSenha.jsx
@@ -1,17 +1,49 @@
+import { ArrowLeft, ArrowRight, Mail, Loader2 } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Mail, ArrowRight, ArrowLeft } from "lucide-react";
+import AuthHeader from "../components/AuthHeader";
 import { Button } from "../components/ui/button";
 import {
   Card,
   CardContent,
-  CardHeader,
   CardFooter,
+  CardHeader,
 } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
-import AuthHeader from "../components/AuthHeader";
+import axios from "axios";
+import { toast } from "sonner";
 
 export default function RecuperarSenha() {
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  const handleRecoverPassword = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await axios.post("api/auth/recuperar-senha", { email });
+      toast.info("Processando sua solicitação...");
+
+      setIsSubmitted(true);
+    } catch (error) {
+      if (error.response && error.response.status === 500) {
+        toast.error("Erro no servidor", {
+          description:
+            "Ocorreu um problema tentar enviar o e-mail. Tente mais tarde.",
+        });
+      } else {
+        setIsSubmitted(true);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-[#F9FAFB] p-4">
       <Card className="w-full max-w-100 bg-white shadow-sm border-slate-200 py-6 px-2">
@@ -26,7 +58,7 @@ export default function RecuperarSenha() {
         </CardHeader>
 
         <CardContent>
-          <form className="space-y-6">
+          <form onSubmit={handleRecoverPassword} className="space-y-6">
             <div className="space-y-2">
               <Label
                 htmlFor="email"
@@ -41,14 +73,25 @@ export default function RecuperarSenha() {
                   type="email"
                   placeholder="nome@marazul.com.br"
                   className="pl-9 bg-[#F8FAFC] border-slate-200 h-11 text-sm focus-visible:ring-slate-300"
+                  value={email}
+                  onChange={(e)=>setEmail(e.target.value)}
                 />
               </div>
             </div>
             <Button
               type="submit"
               className="w-full bg-[#1e293b] hover:bg-[#0f172a] text-white h-12 font-bold flex items-center justify-center gap-2 mt-2 transition-colors cursor-pointer disabled:cursor-not-allowed"
+              disabled={!isEmailValid||isLoading}
             >
-              Enviar Link de Recuperação <ArrowRight className="h-4 w-4 " />
+              {isLoading ? (
+                <>
+                  Enviando... <Loader2 className="h-4 w-4 animate-spin" />
+                </>
+              ) : (
+                <>
+                  Enviar Link de Recuperação <ArrowRight className="h-4 w-4" />
+                </>
+              )}
             </Button>
           </form>
         </CardContent>
