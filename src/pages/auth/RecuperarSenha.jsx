2170690e9db7e9a2a38819eab948e51947b7451c
Ana Malaquias <ana.beat20051@gmail.com>
2026-08-10 12:56:08 -0300

    feat: replace EmailInput with generic Input component in Login and RecuperarSenha pages

--- src/pages/auth/RecuperarSenha.jsx
+++ src/pages/auth/RecuperarSenha.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import AuthHeader from "../../components/AuthHeader";
 import AuthLayout from "../../components/AuthLayout";
-import EmailInput from "../../components/EmailInput";
+import Input from "../../components/Input";
 import SubmitButton from "../../components/SubmitButton";
 import { CardContent, CardFooter, CardHeader } from "../../components/ui/card";
 
@@ -93,10 +93,7 @@ export default function RecuperarSenha() {
       <CardContent>
         <form onSubmit={handleRecoverPassword} className="space-y-6">
           <div className="space-y-2">
-            <EmailInput
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
 
           <SubmitButton
