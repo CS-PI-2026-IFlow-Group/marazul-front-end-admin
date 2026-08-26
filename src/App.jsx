fd7c3b497199ffef079f11d499784662e0393b24
Ana Malaquias <ana.beat20051@gmail.com>
2026-08-25 23:47:08 -0300

    fix: update import paths for components and correct route definitions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import DashboardLayout from "./components/DashboardLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "./components/ui/sonner";
-import CadastroFuncionario from "./pages/api/cadastros/CadastroFuncionario";
-import Funcionarios from "./pages/api/cadastros/Funcionarios";
-import Dashboard from "./pages/api/Dashboard";
-import Frota from "./pages/api/Frota";
-import Relatorios from "./pages/api/Relatorios";
-import Viagens from "./pages/api/Viagens";
+import Dashboard from "./pages/Dashboard";
+import Frota from "./pages/Frota";
+import Relatorios from "./pages/Relatorios";
+import Viagens from "./pages/Viagens";
 import Login from "./pages/auth/Login";
 import RecuperarSenha from "./pages/auth/RecuperarSenha";
 import RedefinirSenha from "./pages/auth/RedefinirSenha";
+import CadastroFuncionario from "./pages/funcionarios/CadastroFuncionario";
+import Funcionarios from "./pages/funcionarios/Funcionarios";
 
 export default function App() {
   return (
@@ -32,11 +32,15 @@ export default function App() {
           <Route path="/frota" element={<Frota />} />
           <Route path="/viagens" element={<Viagens />} />
 
+          <Route path="/funcionarios" element={<Funcionarios />} />
           <Route
-            path="/cadastroFuncionario"
+            path="/funcionarios/cadastro"
             element={<CadastroFuncionario />}
           />
-          <Route path="/funcionarios" element={<Funcionarios />} />
+          <Route
+            path="/funcionarios/editar/:id"
+            element={<CadastroFuncionario isEdicao={true} />}
+          />
 
           <Route path="/relatorios" element={<Relatorios />} />
         </Route>
